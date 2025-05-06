a84fc2b87ebc343f70c6ac84ac2fdb0860c9fc3c
Paco Goze <paco.goze@polytehnique.edu>
2025-05-06 16:05:11 +0200

    blabla

--- llm_kg_extraction/main_multimodal.py
+++ llm_kg_extraction/main_multimodal.py
@@ -1,18 +1,19 @@
 import os
 from openai import AzureOpenAI
-from multimodal_kg_builder import MultimodalFinancialKGBuilder
+from multimodal_kg_builder_iterative import MultimodalFinancialKGBuilder
 from KG_visualizer import KnowledgeGraphVisualizer
 import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
 
-def main(project_name: str, model_name: str):
+def main(project_name: str, model_name: str, dump: bool = False) -> None:
     """
     Main function to extract knowledge graph from a PDF file and visualize it.
     Args:
-        name (str): The name of the project to be used for file paths.
+        project_name (str): The name of the project to be used for file paths.
         model_name (str): The name of the model to be used for extraction.
+        dump (bool, optional): Flag to indicate if the knowledge subgraphs should be saved. Defaults to False.
     """
 
     load_dotenv()
@@ -21,8 +22,11 @@ def main(project_name: str, model_name: str):
     
     builder = MultimodalFinancialKGBuilder(model_name=model_name, deployment_name=deployment_name)
 
-    kg = builder.build_knowledge_graph_from_pdf(pdf_path)
+    kg = builder.build_knowledge_graph_from_pdf(pdf_path, dump=dump)
     builder.save_knowledge_graph(kg, project_name)
+
+    if dump:
+        print("Knowledge subgraphs have been saved.")
 
     visualizer = KnowledgeGraphVisualizer()
     visualizer.visualize(kg)
@@ -33,11 +37,15 @@ def main(project_name: str, model_name: str):
     print(f"Knowledge graph visualization saved to: {output_path}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python run.py <project_name> <model_name>")
+    if len(sys.argv) < 3:
+        print("Usage: python main_multimodal.py <project_name> <model_name> [--dump]")
         sys.exit(1)
+
     project_name = sys.argv[1]
     model_name = sys.argv[2]
+    dump = False
+    if len(sys.argv) == 4 and sys.argv[3] == "--dump":
+        dump = True
 
-    main(project_name, model_name)
-    print(f"Running with project name: {project_name} and model name: {model_name}")
+    main(project_name, model_name, dump=dump)
+        
