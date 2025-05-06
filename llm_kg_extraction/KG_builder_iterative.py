4d59a299031b84216ea38060711d769c79ba90e6
Paco Goze <paco.goze@polytehnique.edu>
2025-05-06 16:12:30 +0200

    blabla

--- llm_kg_extraction/KG_builder_iterative.py
+++ llm_kg_extraction/KG_builder_iterative.py
@@ -3,7 +3,7 @@ import pymupdf
 import json
 
 from typing import List, Dict
-from openai import AzureOpenAI
+from llm_client import AzureOpenAIClient
 from KG_visualizer import KnowledgeGraphVisualizer
 from dotenv import load_dotenv
 from pathlib import Path
@@ -18,7 +18,7 @@ class FinancialKGBuilder:
     iteratively building a graph page by page with merged subgraphs.
     """
     
-    def __init__(self, model_name, deployment_name, ontology_path: str = Path(__file__).resolve().parent / "ontology" / "pekg_ontology.yaml"):
+    def __init__(self, model_name, deployment_name, ontology_path: str =  Path(__file__).resolve().parent / "ontology" / "pekg_ontology.yaml"):
         """
         Initialize the FinancialKGBuilder with the model name and deployment name.
         Args:
@@ -26,30 +26,13 @@ class FinancialKGBuilder:
             deployment_name (str): The name of the deployment in Azure OpenAI.
             ontology_path (str): Path to the ontology file.
         """
+        
         self.model_name = model_name
-        self.client = self.make_client(self.model_name)
+        self.client = AzureOpenAIClient(model_name=model_name)
+
         self.deployment_name = deployment_name
         self.ontology = PEKGOntology(ontology_path)
-
-    @staticmethod
-    def make_client(model_name: str) -> AzureOpenAI:
-        """
-        Create an Azure OpenAI client based on the model name.
-        Args:
-            model_name (str): The name of the model to be used for extraction.
-        Returns:
-            AzureOpenAI: The Azure OpenAI client.
-        """
-        AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT_" + model_name)
-        AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY_" + model_name)
-        AZURE_DEPLOYMENT_NAME = os.getenv("AZURE_DEPLOYMENT_NAME_" + model_name)
-        AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION_" + model_name)
-
-        return AzureOpenAI(
-            api_key=AZURE_OPENAI_API_KEY,
-            api_version=AZURE_OPENAI_API_VERSION,
-            azure_endpoint=AZURE_OPENAI_ENDPOINT
-        )
+        
 
     def extract_text_from_pdf(self, file_path: str) -> List[str]:
         """
